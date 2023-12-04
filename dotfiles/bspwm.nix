889f949c83fd12d13141eded9201e8058a7d00c2
vavakado <vavakado@skiff.com>
2023-12-04 23:27:12 +0200

    added bspwm

---
+++ dotfiles/bspwm.nix
@@ -0,0 +1,28 @@
+{ config, ... }: {
+  
+    home.file = {
+      ".config/bspwm/bspwmrc" = { 
+        text = ''
+          #! /bin/sh
+          pgrep -x sxhkd > /dev/null || sxhkd &
+          #feh --bg-scale /home/vavakado/wall.jpg
+          feh --bg-scale /home/vavakado/Downloads/wallhaven-q2vzd7_1920x1080.png
+          pipewire &
+          picom --config ~/.config/picom/picom.sample.conf &
+          ~/.config/polybar/launch.sh &
+
+          bspc monitor -d I II III IV V VI
+
+          bspc config border_width         3
+          bspc config window_gap           0
+          bspc config focused_border_color '#658594'
+          bspc config normal_border_color  '#1F1F28'
+          bspc config split_ratio          0.52
+          bspc config borderless_monocle   true
+          bspc config gapless_monocle      true
+          bspc config focus_follows_pointer true
+        '';
+        executable = true;
+      };
+    };
+}
--- home.nix
+++ home.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 {
-  imports = [ ./dotfiles/tmux.nix ];
+  imports = [ ./dotfiles/tmux.nix ./dotfiles/bspwm.nix ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "vavakado";
